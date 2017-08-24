f9d8cd9d60bd1ccce148dd3099a01b98b358e103
Josue Paz <josunpaz@outlook.es>
2017-08-23 23:26:28 -0300

    Subo Clases

---
+++ Clases/Clase_1/Ej01_a_15/ConsoleApplication1/Ej01.cs
@@ -0,0 +1,49 @@
+﻿                                using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Ej01
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int mayor=0,
+                menor=0,
+                acumulador=0,
+                promedio=0,
+                i;
+
+                for (i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Ingrese el numero {0}:",i+1);
+                    numeros[i] = int.Parse(Console.ReadLine());
+
+                    if (i == 0)
+                    {
+                        mayor = numeros[i];
+                        menor = numeros[i];
+
+                    }
+
+                    if (mayor < numeros[i])
+                        mayor = numeros[i];
+
+                    if (menor > numeros[i])
+                        menor = numeros[i];
+
+                    acumulador = acumulador + numeros[i];
+                }
+
+                promedio = acumulador / 5;
+
+                Console.WriteLine("\nEl numero mayor es:{0}\nEl numero menor es:{1}\nEl promedio es:{2}\n",mayor,menor,promedio);
+
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ Clases/Clase_1/Ej01_a_15/Ej02/Ej02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej02
+{
+    class Ej02
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            double cuadrado;
+            double cubo;
+
+            Console.WriteLine("Ingrese un numero:");
+            numero = int.Parse(Console.ReadLine());
+
+            cuadrado = Math.Pow(numero, 2);
+            cubo = Math.Pow(numero, 3);
+
+            Console.WriteLine("\nEl cuadrado de {0} es {1}.\nEl cubo de {0} es {2}.\n", numero, cuadrado, cubo);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
